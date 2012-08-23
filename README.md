615fa817ff7296e31961787874d136138a3129ea
DavidMasonIT <david@intheory.co.uk>
2012-08-23 14:28:59 +0100

    Testing

--- README.md
+++
@@ -1,4 +0,0 @@
-hackldn2012
-===========
-
-The Pythia team strikes again. This time with an amazing, super, uber-cool fashion app. Updates coming soon. 
--- app/model/content.py
+++
@@ -1,9 +0,0 @@
-from mongoengine import Document, StringField, ListField, IntField
-# ============================ User ================================ #
-
-class Question(Document):
-    meta = {"collection":"Questions"}
-    question = StringField(required=True)
-    options = ListField(required=True, StringField, default=list)
-    answer = IntField(required=True)
-    
