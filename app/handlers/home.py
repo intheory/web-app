1f7fdc4ab417a702a85e8cd252b695dcef759107
George Eracleous <basketballcy@gmail.com>
2012-08-26 14:22:24 +0100

    Multi-answer questions done. We record user answers in an array and then in the backend we split those into buckets according to how many correct answers are there in the question

--- app/handlers/home.py
+++ app/handlers/home.py
@@ -20,8 +20,18 @@ class EvaluateHomeQuizHandler(base.BaseHandler):
             questions = MiniQuizQuestion.objects
             correct_answers = [question.answer for question in questions]
             count = 0
+            
+            #Put recorded user answers in buckets according to how many answers each
+            #question has.
+            user_answers = []
+            for answer_list in correct_answers:
+                l = []
+                for i in  range(len(answer_list)):
+                    l.append(int(answers.pop(0)))
+                user_answers.append(l)
+            print correct_answers
             for i in xrange(len(questions)):
-                if set(int(answers[i])) == set(correct_answers[i]):
+                if set(user_answers[i]) == set(correct_answers[i]):
                     count += 1
             return (count,)
         except Exception, e:
