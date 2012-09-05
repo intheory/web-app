4c18fbf04e2cbd400ca76b1565acc57be8002640
George Eracleous <basketballcy@gmail.com>
2012-09-05 17:22:06 +0100

    Added columns in admin panel

--- static/js/admin.js
+++ static/js/admin.js
@@ -2,17 +2,26 @@
  * @fileOverview This file contains all the JavaScript functionality for the admin page.
  * 
  */
-    
     // =============================== Listeners =============================== //
     
     $("#moderator-btn").live("click", function() {
         if ($(this).hasClass("disbaled")) return false; //if this user is a moderator do nothing
-	var uid = $(this).attr("uid");
-	IT.post("/admin/users/moderator", {
-               uid : uid
-	}, true, function(response) 
-	{       
-            console.log(response.moderator); 
+    	var uid = $(this).attr("uid");
+    	IT.post("/admin/users/moderator", {
+                   uid : uid
+    	}, true, function(response) 
+    	{       
+                console.log(response.moderator); 
+        });
+    });
+
+    $("#section-colb-select").live("click", function() {
+        //It will get all the nuggets associayed with this section
+        IT.get("/admin/nuggets/get", {
+                   sid : sid
+        }, true, function(response) 
+        {       
+            console.log(response.html);
         });
     });
 
