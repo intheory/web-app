41a7a66cb19d3142569f8c9ad461c2ded33ae51a
George Eracleous <basketballcy@gmail.com>
2012-09-10 14:00:57 +0100

    Added validation for mock test functionality

--- static/js/test.js
+++ static/js/test.js
@@ -7,7 +7,24 @@
     // =============================== Listeners =============================== //
     
     $("td.choice").live("click", function() {
-    	$(this).parent().addClass("success");
+      var choiceRow = $(this).parent();
+      var remainingAnswers = parseInt($(".next").attr("remaining-answers"));
+
+      if (choiceRow.hasClass("success")){
+        choiceRow.removeClass("success");
+        $(".next").attr("remaining-answers", remainingAnswers+1);
+      }
+      else{
+        if (remainingAnswers==0){
+          $("td.choice").parent(".success:first").removeClass("success");
+          choiceRow.addClass("success");        
+        }
+        else{
+          choiceRow.addClass("success");        
+          $(".next").attr("remaining-answers", remainingAnswers-1);
+        }
+      }
+
     });
 
     $("a.next").live("click", function() {
@@ -15,19 +32,25 @@
     	var answers = [];
       var cursor = $(this).attr("cursor");
       var tid = $(this).attr("tid");
-    	choices.each(function(index) {
+    	var remainingAnswers = $(this).attr("remaining-answers");
+      choices.each(function(index) {
     		if ($(this).hasClass("success")){
     			answers.push(index-1);
     		}
 		  });
 
-      IT.post("/test/evaluate", {
-               tid: tid,
-               answers : JSON.stringify(answers),
-               cursor: cursor
-	     }, true, function(response) 
-	     {       
-          $(".hero-unit1").empty().html(response.html);
-        });    	
+      if (remainingAnswers==0){
+        IT.post("/test/evaluate", {
+                 tid: tid,
+                 answers : JSON.stringify(answers),
+                 cursor: cursor
+  	     }, true, function(response) 
+  	     {       
+            $(".hero-unit1").empty().html(response.html);
+          });    	
+      }
+      else{
+        IT.popup.show("asdsa")
+      }
 
     });
