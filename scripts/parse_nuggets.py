aa46700e718f822fd676bee97f1684ae5a1664f9
George Eracleous <basketballcy@gmail.com>
2012-09-11 15:07:49 +0100

    Added new buttons for question page

--- scripts/parse_nuggets.py
+++ scripts/parse_nuggets.py
@@ -5,18 +5,32 @@ Created on 13 Nov 2011
 
 This module parses a txt file containing nuggets and saves them in the db.
 '''
-from src.app.model.content import Nugget
+import os
+from mongoengine import connect
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath("/home/george/intheoryenv/intheory/src/app/model/content.py")))
+os.sys.path.insert(0,parentdir) 
+from model.content import Section, Nugget
+connect("intheory_dev")
 
 f = open("nuggets.txt", "r")
-content = f.readline()
-while (content != "" ):
-	content = content.strip()
-	if content=="nugget":
-		#n = Nugget()
-		title = f.readline().split(',')[1].strip()
-		content = f.readline().split(',')[1].strip()
-		img = f.readline().split(',')[1].strip()
-		section = f.readline().split(',')[1].strip()
+while 1:
+	print 'in'
+	content = f.readline().strip()
+	if len(content) ==0:
+		break # EOF
 
-
+	if content=="section":
+		s_title = f.readline().split(',')[1].strip()
+		s = Section()
+		s.title = s_title
+		content = f.readline().strip()
+		while (content!="section" and len(content)!=0):
+			if content=="nugget":
+				n = Nugget()
+				n.title = f.readline().split(',')[1].strip()
+				n.content = f.readline().split(',')[1].strip()
+				n.img = f.readline().split(',')[1].strip()
+				s.nuggets.append(n)
+			content = f.readline().strip()
+		s.save()
 f.close()
