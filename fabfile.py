0bbd7762cc0a4e907a8a9f240e4d5f5c37adb377
George Eracleous <basketballcy@gmail.com>
2012-09-19 13:31:52 +0100

    Added deployment scripts

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,10 @@ import os
 from fabric.api import *
 from fabric.colors import green
 
-APP_NAME = "noteslib"
+env.project = 'intheory'
+env.user = 'george'
+code_dir = '/www/virtualenv/intheory/src/'
+env.hosts = ['intheory.co.uk:8080']
 
 ####################
 # PROCESS COMMANDS #
@@ -20,3 +23,20 @@ def start():
     '''
     local("python app.py")
     print(green("Server started!"))
+
+
+def restart():
+   sudo('supervisorctl restart tornadoes:')
+
+def pull():
+        with cd(code_dir):
+                sudo("sudo git pull origin master")
+
+def deploy():
+        sudo('supervisorctl stop tornadoes:')
+        pull()
+        sudo('pip install -r pip-reqs.txt')
+        sudo('supervisorctl start tornadoes:')
+
+def status():
+        sudo('supervisorctl status')
