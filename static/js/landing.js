9b7f8bffe5231fead427c4596c85e86105fe70e8
George Eracleous <basketballcy@gmail.com>
2012-09-26 22:53:08 +0100

    Refined redeisgn

--- static/js/landing.js
+++
@@ -1,6 +0,0 @@
-	var navBtns = $(".nav.nav-pills").children();
-    navBtns.each(function(index) {
-      if ($(this).hasClass("active")){
-        $(this).removeClass("active");
-      }
-    });    
--- static/js/test.js
+++ static/js/test.js
@@ -4,13 +4,6 @@
  * @author l </a>
  */
     // =============================== Listeners =============================== //
-    var navBtns = $(".nav.nav-pills").children();
-    navBtns.each(function(index) {
-      if ($(this).hasClass("active")){
-        $(this).removeClass("active");
-      }
-    });    
-    $("li#mock-test-nav").addClass("active");
 
     $("td.choice").live("click", function() {
       var choiceRow = $(this).parent();
