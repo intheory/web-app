1b96084535af91ac370c9a5688844858f71360b0
George Eracleous <basketballcy@gmail.com>
2012-10-25 21:19:14 +0100

    Finalized the landing pages

--- app/handlers/user.py
+++ app/handlers/user.py
@@ -54,9 +54,6 @@ def has_paid(method):
                     self.redirect("/payment")                    
                 else:
                     return method(self, *args, **kwargs)
-            elif isinstance(self, GetClipPageHandler):
-                #if this is the first time a user requests a new HAZARD test and the user has not reached the limit allow it
-                return method(self, *args, **kwargs)
             elif isinstance(self, GetClipPageHandler) and self.current_user and len(HazardPerceptionTest.objects(uid=str(self.current_user.id))) < self.settings['clips_limit']:
                 #if the request is to start a new HAZARD test and the user has not reached the limit allow it
                 return method(self, *args, **kwargs)
