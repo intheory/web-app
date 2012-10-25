f70a84f73e19d89018f29cff2bff1301917b4ce9
George Eracleous <basketballcy@gmail.com>
2012-10-25 21:27:34 +0100

    Finalized the landing pages

--- app/handlers/test.py
+++ app/handlers/test.py
@@ -4,8 +4,8 @@ from app.model.content import Section, Nugget, PractiseTest, MockTest, Test, Que
 from random import shuffle
 from mongoengine.queryset import DoesNotExist
 
-MOCK_TEST_SIZE = 1
-PRACTISE_TEST_SIZE = 1 
+MOCK_TEST_SIZE = 50
+PRACTISE_TEST_SIZE = 20 
 
 
 class CreateNewTestHandler(base.BaseHandler):
